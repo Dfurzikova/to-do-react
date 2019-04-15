b4bce429f97d39d7714322d2312ad37108770080
Dfurzikova <dfurzikova@yandex.ru>
2019-04-15 08:55:38 +0300

    added production build

---
+++ build/precache-manifest.54e66326c458cbf7e86101744c2823bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19075e8ec5a58ee6c59e",
+    "url": "/static/js/main.fb174002.chunk.js"
+  },
+  {
+    "revision": "f52927769bd91fe1f79b",
+    "url": "/static/js/2.6d9f6be0.chunk.js"
+  },
+  {
+    "revision": "19075e8ec5a58ee6c59e",
+    "url": "/static/css/main.849d5925.chunk.css"
+  },
+  {
+    "revision": "a903e39e91f961c60c3a0b93c1ab465d",
+    "url": "/index.html"
+  }
+];
